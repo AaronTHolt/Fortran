68afc0555118740231974ef8a688193c023a5062
Aaron <aaron@arce-rc138-149-dhcp.colorado.edu>
2016-08-22 15:35:13 -0600

    Fix passing allocatable array

--- NeuralNetwork/matrix_ops.f90
+++ NeuralNetwork/matrix_ops.f90
@@ -7,9 +7,16 @@ program test
   !real, dimension(2:,6:), allocatable :: C
   real, allocatable :: A(:,:), B(:,:)
   real, allocatable :: C(:,:)
-  integer :: i, j
+  integer :: rowsA, rowsB, rowsC, colsA, colsB, colsC, i, j
 
   allocate( A(2,3), B(2,3), C(2,6) )
+
+  rowsA = size(A,1)
+  rowsB = size(B,1)
+  rowsC = rowsA
+  colsA = size(A,2)
+  colsB = size(B,2)
+  colsC = colsA + colsB
 
   do i = 1,2
     do j = 1,3
@@ -18,52 +25,48 @@ program test
     end do
   end do
 
-  call hMatrixConcat(A, B, C)
+  ! do i=1,2
+  !   do j=1,6
+  !     write(*,*) C(i,j)
+  !   end do
+  ! end do
+
+  call hMatrixConcat(A, rowsA, colsA, B, rowsB, colsB, C, rowsC, colsC)
+
+  do i=1,2
+    do j=1,6
+      write(*,*) C(i,j)
+    end do
+  end do
 
   deallocate( A, B, C )
 
-contains
-  subroutine hMatrixConcat(A, B, C)
-    ! Takes <=2d matrices A and B and hoizontally concatenates them
-    ! A and B must have the same number of rows
-    implicit none
-    real, dimension(:,:), intent(in) :: A, B
-    !real, dimension(:,:), allocatable :: C
-    real, dimension(:,:), intent(out) :: C
-    integer :: rowsA, rowsB, colsA, colsB, colsC, i, j
-    rowsA = size(A,1)
-    rowsB = size(B,1)
-    colsA = size(A,2)
-    colsB = size(B,2)
-    colsC = colsA + colsB
-    if( rowsA /= rowsB ) then
-      return
-    end if
-
-
-    !do i=1:rowsA
-    !  C(i, :) = [A]
-    !end do
-    !C = [A, B]
-    do i=1,rowsA
-      do j=1,colsA
-        C(i,j) = A(i,j)
-        !write(*,*) C(i,j)
-      end do
-      do j=colsA+1,colsC
-        C(i,j) = B(i,j-3)
-        !write(*,*) C(i,j)
-      end do
-    end do
-
-    !write(*,*) "-----------------"
-
-    do i=1,rowsA
-      do j=1,colsC
-        !write(*,*) C(i,j)
-      end do
-    end do
-
-  end subroutine hMatrixConcat
-
 end program test
+
+
+subroutine hMatrixConcat(A1, rA, cA, B1, rB, cB, C1, rC, cC)
+  ! Takes <=2d matrices A and B and hoizontally concatenates them
+  ! A and B must have the same number of rows
+  implicit none
+  integer, intent(in) :: rA, rB, cA, cB, rC, cC
+  real, intent(in) :: A1(1:rA,1:cA), B1(1:rB,1:cB)
+  real, intent(out) :: C1(1:rC,1:cC)
+  integer :: i, j
+
+  if( rA /= rB ) then
+    write(*,*) rA, " ", rB
+    return
+  end if
+
+  do i=1,rA
+    do j=1,cA
+      C1(i,j) = A1(i,j)
+      !write(*,*) C(i,j)
+    end do
+    do j=cA+1,cC
+      C1(i,j) = B1(i,j-3)
+      !write(*,*) C1(i,j)
+    end do
+  end do
+
+end subroutine hMatrixConcat
