60c735d12d0daf0e06a5d10721a994df731baebd
AaronTHolt <holtat@colorado.edu>
2016-08-23 11:40:48 -0600

    matrix_ops is now a module. Removed test program

--- NeuralNetwork/matrix_ops.f90
+++ NeuralNetwork/matrix_ops.f90
@@ -1,47 +1,8 @@
 
-
-
-program test
+module matrix_ops
   implicit none
-  real, allocatable :: A(:,:), B(:,:)
-  real, allocatable :: C(:,:)
-  integer :: rowsA, rowsB, rowsC, colsA, colsB, colsC, i, j
 
-  allocate( A(2,3), B(2,3), C(2,6) )
-
-  rowsA = size(A,1)
-  rowsB = size(B,1)
-  rowsC = rowsA
-  colsA = size(A,2)
-  colsB = size(B,2)
-  colsC = colsA + colsB
-
-  do i = 1,2
-    do j = 1,3
-      A(i,j) = i*6 + j - 6
-      B(i,j) = i*6 + j - 3
-    end do
-  end do
-
-  ! do i=1,2
-  !   do j=1,6
-  !     write(*,*) C(i,j)
-  !   end do
-  ! end do
-
-  call hMatrixConcat(A, rowsA, colsA, B, rowsB, colsB, C, rowsC, colsC)
-
-  do i=1,2
-    do j=1,6
-      write(*,*) C(i,j)
-    end do
-  end do
-
-  deallocate( A, B, C )
-
-end program test
-
-
+contains
 subroutine hMatrixConcat(A1, rA, cA, B1, rB, cB, C1, rC, cC)
   ! Takes <=2d matrices A and B and hoizontally concatenates them
   ! A and B must have the same number of rows
@@ -68,3 +29,5 @@ subroutine hMatrixConcat(A1, rA, cA, B1, rB, cB, C1, rC, cC)
   end do
 
 end subroutine hMatrixConcat
+
+end module
