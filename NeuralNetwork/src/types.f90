fce8e75aaeb99321f682fea99226e921c59aa481
AaronTHolt <holtat@colorado.edu>
2016-10-24 14:11:10 -0600

    Add data structure for iris data

--- NeuralNetwork/src/types.f90
+++ NeuralNetwork/src/types.f90
@@ -1,10 +1,18 @@
 module types
   implicit none
 
+  ! Made this so I don't have to call size all the time
   type matrix
     real, allocatable :: mat(:,:)
     integer :: rows
     integer :: cols
   end type matrix
+
+  !! s = sepal, p = petal
+  !! t1,t2,t3 combine to make species type
+  type iris_data
+    real :: s_len, s_wid, p_len, p_wid
+    integer :: t1, t2, t3
+  end type iris_row
 
 end module
