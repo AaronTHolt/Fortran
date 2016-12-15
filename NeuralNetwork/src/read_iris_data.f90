8c9d0b17c0c6ea43582dd681b8cbec9c8dd9b55a
AaronTHolt <holtat@colorado.edu>
2016-12-15 13:01:03 -0700

    read_iris now read file into data structure

--- NeuralNetwork/src/read_iris_data.f90
+++ NeuralNetwork/src/read_iris_data.f90
@@ -22,7 +22,7 @@ integer function get_file_length(filepath) result(y)
       write(*,*) reason, "Can't read"
       exit
     else if (reason < 0) then  ! End of file
-      write(*,*) "Number of lines is", y
+      !write(*,*) "Number of lines is", y
       exit
     else                       ! Successfully reads
       y = y + 1
@@ -31,27 +31,36 @@ integer function get_file_length(filepath) result(y)
   end do
   close(1)
 
-  write(*,*) y
+  !write(*,*) y
 
   return
 
 end function get_file_length
 
-subroutine read_iris(filepath, i_data)
+subroutine read_iris(filepath, file_len, i_data)
   !!
   use types
   implicit none
-  type(iris_data), allocatable, intent(inout) :: i_data(:)
-  integer :: file_len, i, reason
+  type(iris_row), allocatable, intent(inout) :: i_data(:)
+  integer, intent(in) :: file_len
+  integer :: i, reason
   character(*), intent(in) :: filepath
 
-  file_len = get_file_length(filepath)
+  ! file_len = get_file_length(filepath)
 
   !! Allocate space for data
+  ! allocate( i_data(file_len) )
 
   !! Read in data
-  ! do i=1:file_len
-  !   read(*,*,iostat=reason)
+  open (unit=99, file=filepath, status='old', action='read')
+  do i=1,file_len
+    read(99,*,iostat=reason) i_data(i)%s_len, i_data(i)%s_wid, i_data(i)%p_len, i_data(i)%p_wid, &
+                              i_data(i)%t1, i_data(i)%t2, i_data(i)%t3
+    ! write(*,*) i_data(i)%s_len, i_data(i)%s_wid, i_data(i)%p_len, i_data(i)%p_wid, &
+    !                           i_data(i)%t1, i_data(i)%t2, i_data(i)%t3
+  end do
+  close(99)
+
 end subroutine read_iris
 
 end module read_iris_data
