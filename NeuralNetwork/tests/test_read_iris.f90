44965d368b93d214279a3a6dd949a5882daa520d
AaronTHolt <holtat@colorado.edu>
2016-12-15 13:00:22 -0700

    add tests for first and last lines on iris file

--- NeuralNetwork/tests/test_read_iris.f90
+++ NeuralNetwork/tests/test_read_iris.f90
@@ -13,13 +13,25 @@ contains
     implicit none
     character(*), intent(in) :: fileName
     integer :: fileLength
+    type(iris_row), allocatable :: i_data(:)
 
     fileLength = get_file_length(fileName)
-    write(*,*) fileLength
+    write(*,*) "Length ? :", (fileLength == 37)
 
+    allocate( i_data(fileLength))
     ! First and last lines
     ! 0.813331	0.692682	0.854032	0.975	0	0	1
     ! 0.744044	0.551523	0.793548	0.901998	0	0	1
+    call read_iris(fileName, fileLength, i_data)
+
+    write(*,*) "First Line :", (i_data(1)%s_len==0.813331), (i_data(1)%s_wid==0.692682), &
+                    (i_data(1)%p_len==0.854032), (i_data(1)%p_wid==0.975), &
+                    (i_data(1)%t1==0), (i_data(1)%t2==0), (i_data(1)%t3==1)
+
+    write(*,*) "Last Line :", (i_data(fileLength)%s_len==0.744044), (i_data(fileLength)%s_wid==0.551523), &
+                    (i_data(fileLength)%p_len==0.793548), (i_data(fileLength)%p_wid==0.901998), &
+                    (i_data(fileLength)%t1==0), (i_data(fileLength)%t2==0), (i_data(fileLength)%t3==1)
+
 
   end subroutine test_read_iris
 
